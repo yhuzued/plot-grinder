b8e691ba5c862c8134dfe3c00dc64b1f66594297
yhuzued <yusufmait@gmail.com>
2026-03-08 14:40:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './',
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
